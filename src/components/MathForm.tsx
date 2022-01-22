27596ab700bd69bb332fa9c9140eb0eba6bd9e74
Omar Sayed <orhman@hotmail.com>
2022-01-22 21:57:19 +0200

    updated css

--- src/components/MathForm.tsx
+++ src/components/MathForm.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Form, Button, Dropdown, DropdownButton, Col, Row, ButtonGroup} from 'react-bootstrap';
+import { Form, Button, Dropdown, DropdownButton, Col, Row, ButtonGroup, Container} from 'react-bootstrap';
 import { getValue, getMessage, add, sub, mul, div } from '../features/math/mathFormSlice';
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 export default function MathForm() {
@@ -34,57 +34,72 @@ export default function MathForm() {
       
     }
   return (
-    <Form as={Row}>
-      <Form.Group as={Col} sm="5" className="mb-3" controlId="firstInput">
-        <Form.Label>Input1</Form.Label>
-        <Form.Control
-          type="number"
-          placeholder="Enter number"
-          required
-          value={input1}
-          onChange={(e) => setInput1(parseFloat(e.target.value))}
-        />
-        <Form.Text className="text-muted">First Number of Operation</Form.Text>
-      </Form.Group>
-      <Form.Group as={Col} sm="2" className="align-self-center" required>
-        <DropdownButton
-          onSelect={(e) => setOperation(e)}
-          id="operation"
-          title="Operation"
-        >
-          <Dropdown.ItemText>Choose Operation</Dropdown.ItemText>
-          <Dropdown.Item eventKey="Add">Add</Dropdown.Item>
-          <Dropdown.Item eventKey="Sub">Sub</Dropdown.Item>
-          <Dropdown.Item eventKey="Mul">Mul</Dropdown.Item>
-          <Dropdown.Item eventKey="Div">Div</Dropdown.Item>
-        </DropdownButton>
-        <Form.Label className="mt-1">{operation}</Form.Label>
-      </Form.Group>
-
-      <Form.Group as={Col} sm="5" className="mb-3" controlId="secondInput">
-        <Form.Label>Input2</Form.Label>
-        <Form.Control
-          type="number"
-          placeholder="Enter number"
-          required
-          value={input2}
-          onChange={(e) => setInput2(parseFloat(e.target.value))}
-        />
-        <Form.Text className="text-muted">Second Number of Operation</Form.Text>
-      </Form.Group>
-      <Col sm="6" className="m-auto mt-5 mb-5">
-        <ButtonGroup className="d-flex">
-          <Button onClick={handleSubmit} variant="primary" type="submit">
-            Submit
-          </Button>
-        </ButtonGroup>
-      </Col>
+    <Container>
       <Row>
-        <Col sm="6" className="m-auto mt-5 mb-5">
-          <p>{message}</p>
-          <h2>{value}</h2>
+        <Col lg="8" className="shadow pt-5 rounded m-auto">
+          <Form as={Row}>
+            <Form.Group as={Col} sm="4" className="mb-3" controlId="firstInput">
+              <Form.Label>Input1</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter number"
+                required
+                value={input1}
+                onChange={(e) => setInput1(parseFloat(e.target.value))}
+              />
+              <Form.Text className="text-muted">
+                First Number of Operation
+              </Form.Text>
+            </Form.Group>
+            <Form.Group as={Col} sm="4" className="align-self-center" required>
+              <DropdownButton
+                onSelect={(e) => setOperation(e)}
+                id="operation"
+                title="Operation"
+              >
+                <Dropdown.ItemText>Choose Operation</Dropdown.ItemText>
+                <Dropdown.Item eventKey="Add">Add</Dropdown.Item>
+                <Dropdown.Item eventKey="Sub">Sub</Dropdown.Item>
+                <Dropdown.Item eventKey="Mul">Mul</Dropdown.Item>
+                <Dropdown.Item eventKey="Div">Div</Dropdown.Item>
+              </DropdownButton>
+              <Form.Label className="mt-1">{operation}</Form.Label>
+            </Form.Group>
+
+            <Form.Group
+              as={Col}
+              sm="4"
+              className="mb-3"
+              controlId="secondInput"
+            >
+              <Form.Label>Input2</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter number"
+                required
+                value={input2}
+                onChange={(e) => setInput2(parseFloat(e.target.value))}
+              />
+              <Form.Text className="text-muted">
+                Second Number of Operation
+              </Form.Text>
+            </Form.Group>
+            <Col sm="6" className="m-auto mt-5 mb-2">
+              <ButtonGroup className="d-flex">
+                <Button onClick={handleSubmit} variant="primary" type="submit">
+                  Submit
+                </Button>
+              </ButtonGroup>
+            </Col>
+            <Row>
+              <Col sm="6" className="m-auto mt-5 mb-5">
+                <p>{message}</p>
+                <h2>{value}</h2>
+              </Col>
+            </Row>
+          </Form>
         </Col>
       </Row>
-    </Form>
+    </Container>
   );
 }
